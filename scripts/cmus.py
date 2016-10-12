f1facd0c1e66794f871ec538f8880f0e61307355
jFilteau <justin_filteau@hotmail.com>
2016-10-12 14:28:57 -0400

    cmus script display only if a song if playing

--- scripts/cmus.py
+++ scripts/cmus.py
@@ -6,8 +6,10 @@ process_name = 'cmus'
 for p in psutil.process_iter():
 	if p.name() == process_name:
 		p = subprocess.Popen('cmus-remote -Q', stdout=subprocess.PIPE, shell=True)
-		p_info = p.stdout.read()
-		artist = p_info.splitlines()[4]
-		title = p_info.splitlines()[5]
+		p_info = p.stdout.read().splitlines()
+		status = p_info[0][7:]
+		artist = p_info[4]
+		title = p_info[5]
 		song_info = artist[11:] + ' - ' + title[10:]
-		os.system('echo ' + song_info)
+		if status == 'playing':
+			os.system('echo ' + song_info)
